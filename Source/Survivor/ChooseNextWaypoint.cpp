03096a3c7d826a9260d31a5538433d4935369081
DESKTOP-FADLH1M\Jader Henryk <jaderhenryk@gmail.com>
2018-04-28 13:22:43 -0300

    Creating Thrid Person Character Patrolling Guard Class in C++

--- Source/Survivor/ChooseNextWaypoint.cpp
+++ Source/Survivor/ChooseNextWaypoint.cpp
@@ -2,9 +2,12 @@
 
 #include "Survivor.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI C++ tasks is running"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index : %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
--- Source/Survivor/ChooseNextWaypoint.h
+++ Source/Survivor/ChooseNextWaypoint.h
@@ -14,5 +14,10 @@ class SURVIVOR_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
 	
 };
