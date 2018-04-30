0abad37addc16ee5ad6f4a83eb857e1a18783193
DESKTOP-FADLH1M\Jader Henryk <jaderhenryk@gmail.com>
2018-04-30 20:08:52 -0300

    Finishing to convert AI Patrolling Behaviour from blueprint to C++

--- Source/Survivor/ChooseNextWaypoint.cpp
+++ Source/Survivor/ChooseNextWaypoint.cpp
@@ -2,12 +2,26 @@
 
 #include "Survivor.h"
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	//Get patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index : %i"), Index);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	
 	return EBTNodeResult::Succeeded;
 }
--- Source/Survivor/PatrollingGuard.h
+++ Source/Survivor/PatrollingGuard.h
@@ -13,6 +13,7 @@ class SURVIVOR_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public:
 	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 	TArray<AActor*> PatrolPointsCPP;
 	
