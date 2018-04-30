49427617361c1699bdb397f57a1fb1d353334bad
DESKTOP-FADLH1M\Jader Henryk <jaderhenryk@gmail.com>
2018-04-30 20:57:15 -0300

    Deleting PatrollingGuard Class and Creating PatrolRoute Actor Component.

--- Source/Survivor/ChooseNextWaypoint.cpp
+++ Source/Survivor/ChooseNextWaypoint.cpp
@@ -3,16 +3,24 @@
 #include "Survivor.h"
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	//Get patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) 
+	{
+		return EBTNodeResult::Failed;
+	}
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing Patrol Points!"));
+		return EBTNodeResult::Failed;
+	}
 	
 	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
--- Source/Survivor/PatrolRoute.cpp
+++ Source/Survivor/PatrolRoute.cpp
@@ -1,8 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Survivor.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 
 
-
+TArray<AActor*> UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
